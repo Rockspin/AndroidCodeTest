297aff78d7029366813f9f5bd6a2581896bd7a6e
Светломир Желев <szhelev@amroncorp.com>
2017-01-08 16:49:03 +0200

    1) Fixed build and runtime errors 2) Optimized no internet connection case for the user

--- app/src/main/java/com/rockspin/androiddevtest/MainActivity.java
+++ app/src/main/java/com/rockspin/androiddevtest/MainActivity.java
@@ -1,18 +1,27 @@
 package com.rockspin.androiddevtest;
 
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import java.util.List;
-
-import retrofit2.Response;
-
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private EVActivityAdapter mAdapter;
+    private RecyclerView        mRecyclerView   = null;
+    private EVActivityAdapter   mAdapter        = null;
+    private ProgressDialog      m_loadingDialog = null;
+        private AlertDialog     m_alertDialog   = null;
+
+    private BroadcastDataUpdateReceiver m_brcUpdateData = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +34,70 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new EVActivityAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
 
-        final Response<List<CosmonautActivity>> response = MyApplication.getAPIService().getEVList().execute();
-        final List<CosmonautActivity> cosmonautActivityList = response.body();
-        mAdapter.setCosmonautActivityList(cosmonautActivityList);
+
+        m_brcUpdateData = new BroadcastDataUpdateReceiver();
+        registerReceiver( m_brcUpdateData, new IntentFilter( MyApplication.BROADCAST_UPDATE_DATA_HANDLER ) );
+
+        if( hasInternetConnection() )
+        {
+            fetchData();
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder( this );
+        builder.setMessage( getText( R.string.no_net_connection ) );
+        builder.setPositiveButton(getText(R.string.retry), new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                m_alertDialog.dismiss();
+                fetchData();
+            }
+        });
+
+        m_alertDialog = builder.create();
+        m_alertDialog.setCancelable( false );
+        m_alertDialog.show();
+    }
+
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        try
+        {
+            if( m_brcUpdateData != null )
+                unregisterReceiver( m_brcUpdateData );
+        }
+        catch( IllegalArgumentException e )
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean hasInternetConnection()
+    {
+        ConnectivityManager conManager  = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo         netInfo     = conManager.getActiveNetworkInfo();
+
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    private void fetchData()
+    {
+        m_loadingDialog = ProgressDialog.show(MainActivity.this, "", getText(R.string.fetch_data), true);
+
+        APICommThread appCommThread = new APICommThread();
+        appCommThread.start();
+    }
+
+    class BroadcastDataUpdateReceiver extends BroadcastReceiver
+    {
+        @Override
+        public void onReceive( Context context, Intent intent )
+        {
+            m_loadingDialog.dismiss();
+            mAdapter.setCosmonautActivityList( MyApplication.instance().getCosmonautActivities() );
+        }
     }
 }
